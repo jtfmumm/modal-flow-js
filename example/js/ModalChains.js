1c468d55c3061fe21c30d058b759a2e467469a4f
John Mumm <jtfmumm@gmail.com>
2015-10-09 13:59:18 -0400

    Create data passing modal chain example

--- example/js/ModalChains.js
+++ example/js/ModalChains.js
@@ -1,33 +1,4 @@
 
-//CoolModal = ModalFlow.buildModalWith({
-//    initialize: function(options) {
-//        console.log("Initializing...");
-//    },
-//    ajaxParameters: {
-////        type: "POST",
-////        url: PATH_PREFIX + "ajax/create-account",
-//        data: function() {
-////            var fields = $form.serializeArray();
-////            fields.push({name: "cta", value: cta});
-////            return fields;
-//        },
-//        responseMixin: function() {
-////            var jobTitle = $("[data-js=bespoke-page__title]").text();
-////            return {jobTitle: jobTitle, cta: cta};
-//        },
-//        error: function(errors) {
-////            JSON.parse(errors.responseText).forEach(function(error) {
-////                console.log("Error: " + error.field);
-////            });
-//        }
-//    },
-//    $form: $("#cool-form"),
-//    $modal: $("#cool-modal"),
-//    beforeSubmit: function() {
-//        console.log("About to submit...");
-//    }
-//});
-
 DataPassingModal1 = ModalFlow.buildModalWith({
     initialize: function(options) {
         console.log("Initializing step 1...");
@@ -35,6 +6,7 @@ DataPassingModal1 = ModalFlow.buildModalWith({
     clickthroughDataUpdater: function() {
         var data = {};
         data.step1 = $("#step-1-input").val();
+        $("#step-1-input").val("");
         return data;
     },
     $modal: $("#modal-step-1"),
@@ -51,6 +23,7 @@ DataPassingModal2 = ModalFlow.buildModalWith({
     },
     clickthroughDataUpdater: function(data) {
         data.step2 = $("#step-2-input").val();
+        $("#step-2-input").val("");
         return data;
     },
     $modal: $("#modal-step-2"),
@@ -64,7 +37,12 @@ var dataPassingModalExit = function(data) {
     console.log("Words: ");
     console.log("Step 1: " + data.step1);
     console.log("Step 2: " + data.step2);
+    closeModal();
 };
+
+function closeModal() {
+    $("[data-js=close-modal]").click();
+}
 
 var dataPassingModalChain = ModalFlow.chain(DataPassingModal1, DataPassingModal2, dataPassingModalExit);
 
